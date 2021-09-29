1048af04c8a38f738a98c5c477ca45d48be91271
jummi <98qkrwjdal@naver.com>
2021-09-30 05:34:30 +0900

    feat: 새로운 product 생성 기능과 form submit 구현

--- src/main/java/com/example/gccoffee/controller/ProductController.java
+++ src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.gccoffee.model.Product;
 import com.example.gccoffee.service.ProductService;
@@ -23,5 +24,16 @@ public class ProductController {
         List<Product> products = productService.getAllProducts();
         model.addAttribute("products", products);
         return "product-list";
+    }
+
+    @GetMapping("/new-product")
+    public String newProductPage() {
+        return "new-product";
+    }
+
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest request) {
+        productService.createProduct(request.productName(), request.category(), request.price(), request.description());
+        return "redirect:/products";
     }
 }
