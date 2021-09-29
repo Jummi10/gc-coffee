59279cd5d58a200d99e0d2376e5fb43af36b088c
jummi <98qkrwjdal@naver.com>
2021-09-30 04:35:33 +0900

    feat: product service 생성, 조회 및 controller 전체 상품 조회, 상품 조회 페이지 구현

---
+++ src/main/java/com/example/gccoffee/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.gccoffee.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.gccoffee.model.Product;
+import com.example.gccoffee.service.ProductService;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+}
