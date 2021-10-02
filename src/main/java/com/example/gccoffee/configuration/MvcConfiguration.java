a83acd44f5c9bd61fbbc42f38bdf79cf60389a0b
jummi <98qkrwjdal@naver.com>
2021-10-02 15:42:36 +0900

    style: 오타 수정

--- src/main/java/com/example/gccoffee/configuration/MvcConfiguration.java
+++ src/main/java/com/example/gccoffee/configuration/MvcConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/*")
+        registry.addMapping("/api/**")
             .allowedOrigins("*");
     }
 }
